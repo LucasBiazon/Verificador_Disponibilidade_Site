2b46cb75c290e07de00ce3089ead11b9c1748ba6
LucasBiazon <lucasbiazonpalma@gmail.com>
2025-02-09 08:15:28 -0300

    Feat: cli init

--- main.go
+++ main.go
@@ -1,5 +1,48 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+
+func Espace(){
+	fmt.Print("\n\n\n")
+}
+
 func main(){
-	
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("Enter some text (press Ctrl+D or Ctrl+Z to end):")
+	fmt.Println("\t Digite 1 - para abrir interface\n" +
+							"\t Digite 2 - para abrir arquivo de entrada\n" +
+							"\t Digite 3 - para abrir arquivo de resposta")
+	fmt.Print("Your choice: ")
+	for scanner.Scan(){
+		text := scanner.Text()
+
+		Espace()
+
+		switch text{
+			case "1":
+				fmt.Println(" -Iniciando server...")
+				fmt.Println(" -Abrindo interface...")
+			case "2":
+				fmt.Println(" -Abrindo arquivo de entrada...")
+			case "3": 
+				fmt.Println(" -Abrindo arquivo de respostas...")
+			default:
+				fmt.Println("Ops... Não há essa opção")
+				fmt.Println("\t Digite 1 - para abrir interface\n" +
+							"\t Digite 2 - para abrir arquivo de entrada\n" +
+							"\t Digite 3 - para abrir arquivo de resposta")
+		}
+
+		Espace()
+		fmt.Print("Your new choice: ")
+
+		if err := scanner.Err(); err != nil {
+			fmt.Println("Error:", err)
+		}
+	}
 }
